1ccbf2a3ccf7bae0ec6c4eba81a5c02f3cf1a990
Alfred Ikiugu <alfred.ikiugu@softwaregroup.com>
2021-04-03 19:34:31 +0300

    screen and android setup complete

---
+++ lib/screens/scanner.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Scanner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Blue Scan'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+    );
+  }
+}
